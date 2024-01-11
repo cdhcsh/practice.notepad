c230a194aacbad8243fb9b09f4d32fbc81c9cd8f
cdhcsh <cdhcsh@gmail.com>
2024-01-11 23:59:32 +0900

    default file create

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'this is Home!'
+    return render_template('index.html')
 @app.route("/mypage")
 def mypage():
     return 'This is My page!'
