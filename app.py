294744457fe2f686cb248d5002157274ca3d60cb
최동환 <cdhcsh@gmail.com>
2024-01-13 20:35:59 +0900

    프론트엔드 ajax 테스트

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request,pymongo
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
-@app.route("/mypage")
-def mypage():
-    return 'This is My page!'
+
+@app.route("/memo",methods=['GET'])
+def get_articles():
+    
+    return jsonify({'result':'success','msg':'GET'});
+
+@app.route("/memo",methods=['POST'])
+def post_articles():
+    return jsonify({'result':'success','msg':'POST'});
+
+
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
